ad5ea10a72cfd705573a0cf523a25af061fdc51d
tenorioDevelop <123768019+tenorioDevelop@users.noreply.github.com>
2024-02-29 17:53:55 +0100

    Instalacion crud automatico y creado de producto

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,10 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Product
+ *
+ * @property $id
+ * @property $nombre
+ * @property $descripcion
+ * @property $imagen
+ * @property $precio
+ * @property $stock
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
 class Product extends Model
 {
-    use HasFactory;
+    
+    static $rules = [
+		'nombre' => 'required|string',
+		'descripcion' => 'required|string',
+		'imagen' => 'required|string',
+		'precio' => 'required',
+		'stock' => 'required',
+    ];
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['nombre','descripcion','imagen','precio','stock'];
+
+
+
 }
